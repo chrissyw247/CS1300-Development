ccaecaa9cf36d2597965b26ac31b9ec857e254d6
Christine Wang <christinewangcw@gmail.com>
2018-12-06 00:53:05 -0500

    Updates

---
+++ precache-manifest.c0a951991a55139aab9778c091858aaf.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cfea630643f8d53ff8c4468f61bbdd0",
+    "url": "/CS1300-Development/static/media/dead.9cfea630.jpg"
+  },
+  {
+    "revision": "205a69b003c72c9486ad",
+    "url": "/CS1300-Development/static/css/main.e9c4002b.chunk.css"
+  },
+  {
+    "revision": "090aa323d9ee3e14764f",
+    "url": "/CS1300-Development/static/js/1.090aa323.chunk.js"
+  },
+  {
+    "revision": "4c226e876d1ae1a08641",
+    "url": "/CS1300-Development/static/js/runtime~main.4c226e87.js"
+  },
+  {
+    "revision": "6a133ad41786c506e572cae7a060bea2",
+    "url": "/CS1300-Development/static/media/alice.6a133ad4.jpg"
+  },
+  {
+    "revision": "41f5165ce70de248c95c41be91f10e52",
+    "url": "/CS1300-Development/static/media/apoc.41f5165c.jpg"
+  },
+  {
+    "revision": "8ff3e3c45aa412cba907fd875433ec84",
+    "url": "/CS1300-Development/static/media/atomic.8ff3e3c4.jpg"
+  },
+  {
+    "revision": "58ced017cccfcc0218e33a0f354fade2",
+    "url": "/CS1300-Development/static/media/baby.58ced017.jpg"
+  },
+  {
+    "revision": "0b9b1a6a219de5a55dfafffff3e36846",
+    "url": "/CS1300-Development/static/media/batman.0b9b1a6a.jpg"
+  },
+  {
+    "revision": "fb6807f011b71184533a473a759b24a9",
+    "url": "/CS1300-Development/static/media/beauty.fb6807f0.jpg"
+  },
+  {
+    "revision": "938932b578cc25752e27240a09fa92f5",
+    "url": "/CS1300-Development/static/media/call.938932b5.png"
+  },
+  {
+    "revision": "a41f63732b18589468c51b03ba09f8ff",
+    "url": "/CS1300-Development/static/media/candles.a41f6373.jpg"
+  },
+  {
+    "revision": "670510d6e70a7a5eb18074e146b141b7",
+    "url": "/CS1300-Development/static/media/cars.670510d6.jpg"
+  },
+  {
+    "revision": "445a471cd38380f7d2592c3628263457",
+    "url": "/CS1300-Development/static/media/catch.445a471c.jpg"
+  },
+  {
+    "revision": "fbb4aad5a8b4288d59fd060acdcc4a89",
+    "url": "/CS1300-Development/static/media/crazy.fbb4aad5.jpg"
+  },
+  {
+    "revision": "205a69b003c72c9486ad",
+    "url": "/CS1300-Development/static/js/main.205a69b0.chunk.js"
+  },
+  {
+    "revision": "1178a41136503ae2535c2318fcb5a666",
+    "url": "/CS1300-Development/static/media/incredibles.1178a411.jpeg"
+  },
+  {
+    "revision": "0e3a7eddd5bddff1f5b0ecde71a1b80d",
+    "url": "/CS1300-Development/static/media/lion.0e3a7edd.jpg"
+  },
+  {
+    "revision": "596c2e1b33bc43573be8021cf263442e",
+    "url": "/CS1300-Development/static/media/logo.596c2e1b.png"
+  },
+  {
+    "revision": "2cfec9aab0ab11836d2631a65067e8ea",
+    "url": "/CS1300-Development/static/media/monsters.2cfec9aa.jpg"
+  },
+  {
+    "revision": "d619479a9dd3db8fb27e00eaa76bfd72",
+    "url": "/CS1300-Development/static/media/neighbors.d619479a.jpg"
+  },
+  {
+    "revision": "4e4313301d1dcf22cd916dc5d9703414",
+    "url": "/CS1300-Development/static/media/nemo.4e431330.jpg"
+  },
+  {
+    "revision": "159bd5d5230e0c065a48e3f0089babb5",
+    "url": "/CS1300-Development/static/media/pacific.159bd5d5.jpeg"
+  },
+  {
+    "revision": "762228f62e27a5e317bece3f03d13369",
+    "url": "/CS1300-Development/static/media/pulp.762228f6.jpg"
+  },
+  {
+    "revision": "2fe9eb3da7b4602debca1716729b01b1",
+    "url": "/CS1300-Development/static/media/rat.2fe9eb3d.jpg"
+  },
+  {
+    "revision": "74a093a4231b3a4cfea79e8d39d851cf",
+    "url": "/CS1300-Development/static/media/star.74a093a4.jpg"
+  },
+  {
+    "revision": "a5ca920cf6bfec2d979981eb6caaa44d",
+    "url": "/CS1300-Development/static/media/super.a5ca920c.jpg"
+  },
+  {
+    "revision": "f2c73ecadc87fd698bd27333a8dd69f2",
+    "url": "/CS1300-Development/static/media/tarzan.f2c73eca.jpg"
+  },
+  {
+    "revision": "c9b68fca2dd8342b161661a83872f9a3",
+    "url": "/CS1300-Development/static/media/zoo.c9b68fca.jpg"
+  },
+  {
+    "revision": "bb707c6be53a45314ab71a75d4dd7404",
+    "url": "/CS1300-Development/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/CS1300-Development/precache-manifest.c0a951991a55139aab9778c091858aaf.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/CS1300-Development/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
